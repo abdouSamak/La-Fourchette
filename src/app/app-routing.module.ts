cfb63c9a18c6d9747aa1e44b28bd38f55eba9ded
abdou <abdou.samake0908@gmail.com>
2020-08-28 18:06:31 +0200

    add files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {InformationsComponent} from "./component/informations/informations.component";
+import {FirstComponent} from "./component/first/first.component";
+import {AcceuilComponent} from "./component/acceuil/acceuil.component";
+import {ReservationComponent} from "./component/reservation/reservation.component";
+import {MenuComponent} from "./component/menu/menu.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'acceuil', component: AcceuilComponent},
+  {path: 'information', component: InformationsComponent},
+  {path: 'menu', component: MenuComponent},
+  {path: 'reservation', component: ReservationComponent},
+  { path: '',   redirectTo: '/acceuil', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,41 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {NgZorroAntdModule, NzLayoutModule} from "ng-zorro-antd";
+import {FormsModule} from "@angular/forms";
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { MenuComponent } from './component/menu/menu.component';
+import {InformationsComponent} from "./component/informations/informations.component";
+import {FirstComponent} from "./component/first/first.component";
+import {ReservationComponent} from "./component/reservation/reservation.component";
+import {AcceuilComponent} from "./component/acceuil/acceuil.component";
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AcceuilComponent,
+    InformationsComponent,
+    MenuComponent,
+    ReservationComponent,
+    FirstComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgZorroAntdModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
